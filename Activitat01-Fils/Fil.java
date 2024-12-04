1c709668a14da939d050fd5c370a47ee392fbbcb
sunrise <alba.segurallamas@gmail.com>
2024-12-04 20:28:48 +0100

    Activitat fils exercici 3

--- Activitat01-Fils/Fil.java
+++ Activitat01-Fils/Fil.java
@@ -8,6 +8,11 @@ public class Fil extends Thread{
     public void run() {
         for(int i = 1; i < 10; i++){
             System.out.println(name + " " + i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
         System.out.println("Termina el fil " + name);
         
