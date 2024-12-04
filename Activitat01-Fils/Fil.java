766ed6bf77d6d94289ac952e04f63f06684abdd8
sunrise <alba.segurallamas@gmail.com>
2024-12-04 20:15:15 +0100

    Activitat fils exercici 3

--- Activitat01-Fils/Fil.java
+++ Activitat01-Fils/Fil.java
@@ -7,7 +7,7 @@ public class Fil extends Thread{
     @Override
     public void run() {
         try {
-            Thread.sleep(100000);
+            Thread.sleep(100);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
