fbdb514e326dc6fda0398cc7730a2e7dcdd88016
sunrise <alba.segurallamas@gmail.com>
2024-12-04 20:25:03 +0100

    Activitat fils exercici 2

--- Activitat01-Fils/Principal.java
+++ Activitat01-Fils/Principal.java
@@ -2,6 +2,8 @@ public class Principal{
     public static void main(String[] args){
         Fil fil1 = new Fil("Juan");
         Fil fil2 = new Fil("Pepe");
+        fil1.setPriority(Thread.MAX_PRIORITY); 
+        fil2.setPriority(Thread.MIN_PRIORITY);
         fil1.start();
         fil2.start();
         System.out.println("Termina thread main");
