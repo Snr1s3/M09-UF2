177e457d9786ecacaa4a4265feec345ad6f8d321
sunrise <alba.segurallamas@gmail.com>
2025-01-15 20:54:32 +0100

    Activita03 Sleep_i_coet feina de classe

--- Activitat03-Sleep_i_Coet/sleepcoet/src/main/java/Coet.java
+++ Activitat03-Sleep_i_Coet/sleepcoet/src/main/java/Coet.java
@@ -1,62 +1,54 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Coet {
     public static Motor[] motors;
     public static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     public static int numPropulsors = 4;
-    public static int velocitatObjectiu = 1;
+    public static int velocitatObjectiu = 0;
     public static void main(String[] args){
         motors = new Motor[numPropulsors];
         for(int i = 0; i< numPropulsors; i++){
             motors[i] = new Motor("Motor "+i);
         }
-        while(velocitatObjectiu > 0){
-            velocitatObjectiu = isInt();
-            while(!passaApotencia(velocitatObjectiu)){
-                velocitatObjectiu = isInt();
-            }
-            if(velocitatObjectiu != 0){ 
-                System.out.println("Passant a potència: "+ velocitatObjectiu);
-                for (int i = 0; i < numPropulsors; i++) {
-                    motors[i].setPotencia(velocitatObjectiu);
-                }
-            }
+        velocitatObjectiu = readUserInput();
+        if(velocitatObjectiu == 0){
+            return;
         }
-    }
-    public static boolean passaApotencia(int p){
-        if(p >=0 && 10 >= p){
-            return true;
+        passaAPotencia(velocitatObjectiu);
+        for (Motor motor : motors) {
+            motor.start();
         }
-        else{
-            System.out.println("Nombre invàlid!!");
-            return false;
+        while(velocitatObjectiu != 0){
+            velocitatObjectiu = readUserInput();
+            passaAPotencia(velocitatObjectiu);
         }
     }
 
-    public static String readUserInput() {
-        try {
-            String line = reader.readLine();
-            if (line == null) {
-                throw new RuntimeException("Line == Null");
+    public static void passaAPotencia(int p) {
+        if (p >= 0 && p <= 10) {
+            for (Motor motor : motors) {
+                motor.setPotencia(p);
+                
             }
-            return line;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        System.out.println("Passant a potència " + p);
+        } else {
+            System.out.println("Error: La potència ha de ser entre 0 i 10.");
         }
     }
-
-    public static int isInt() {
+    public static int readUserInput() {
         int num = 0;
         boolean validNum = false;
         while (!validNum) {
             try {
-                String userInput = readUserInput();
-                num = Integer.parseInt(userInput);
+                String line = reader.readLine();
+                if (line == null) {
+                    throw new RuntimeException("Line == Null");
+                }
+                num = Integer.parseInt(line);
                 validNum = true;
-            } catch (NumberFormatException e) {
-                System.out.println("L'entrada no és un número vàlid.");
+            } catch (Exception e) {
+                throw new RuntimeException(e);
             }
         }
         return num;
--- Activitat03-Sleep_i_Coet/sleepcoet/src/main/java/Motor.java
+++ Activitat03-Sleep_i_Coet/sleepcoet/src/main/java/Motor.java
@@ -1,31 +1,51 @@
-
 public class Motor extends Thread {
     private int objectiu;
-    private int actual = 0;
+    private int actual;
+
     public Motor(String name) {
         super(name);
+        this.objectiu = 0;
+        this.actual = 0;
     }
-    public synchronized void setPotencia(int p) {
+
+    public void setPotencia(int p) {
         this.objectiu = p;
-        new Thread(() -> {
-            while(objectiu != actual){
-                if(objectiu > actual){
-                    actual++;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                while (objectiu != actual) {
+                    if (objectiu > actual) {
+                        actual++;
+                        System.out.println(getString(2));
+                    } else if (objectiu < actual) {
+                        actual--;
+
+                    System.out.println(getString(1));
+                    }
+                    Thread.sleep((long) (Math.random() * 1000) + 1000); // Temps aleatori entre 1000 i 2000 mil·lisegons (1 i 2 segons)
                 }
-                else if(objectiu < actual){
-                    actual--;
-                }
-                System.out.println(getString());
-                try{
-                    Thread.sleep((long) (Math.random() *2000));
-                }
-                catch(InterruptedException e) {
-                    e.printStackTrace();
+                Thread.sleep(100);
+                //System.out.println(getName()+"hola");
+                if (objectiu == 0 && actual == 0) {
+                    System.out.println(getName() + " ha arribat a la potència 0 i surt.");
+                    break;
                 }
             }
-        }).start();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
-    public String getString() {
-        return getName()+ " Incre. Objectiu: "+ objectiu + " Actual: "+actual;
+
+    public String getString(int n) {
+        if(n == 1){
+            return getName() + " Decre. Objectiu: " + objectiu + " Actual: " + actual;
+        }
+        else{
+            return getName() + " Incre. Objectiu: " + objectiu + " Actual: " + actual;
+        }
+        
     }
 }
